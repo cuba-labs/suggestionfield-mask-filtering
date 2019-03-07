63bbfe02882b4ff1c3bfdc4043d22574cd64bddc
Daniil Tsarev <tsarev@haulmont.com>
2019-03-07 10:01:56 +0400

    Implement mask filtering

--- modules/web/src/com/company/sample/web/SampleScreen.java
+++ modules/web/src/com/company/sample/web/SampleScreen.java
@@ -1,10 +1,31 @@
 package com.company.sample.web;
 
-import com.haulmont.cuba.gui.screen.Screen;
-import com.haulmont.cuba.gui.screen.UiController;
-import com.haulmont.cuba.gui.screen.UiDescriptor;
+import com.company.sample.entity.NameEntity;
+import com.haulmont.cuba.gui.model.CollectionContainer;
+import com.haulmont.cuba.gui.screen.*;
+
+import javax.inject.Inject;
+import java.util.List;
+import java.util.Map;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 @UiController("sample_SampleScreen")
 @UiDescriptor("sample-screen.xml")
+@LoadDataBeforeShow
 public class SampleScreen extends Screen {
+
+    @Inject
+    private CollectionContainer<NameEntity> names;
+
+    @Install(to = "suggestionField", subject = "searchExecutor")
+    private List suggestionFieldSearchExecutor(String query, Map<String, Object> params) {
+        // consider a query with dot "." like regexp
+        Pattern likeQuery = Pattern.compile(query);
+
+        return names.getItems()
+                .stream()
+                .filter(item -> likeQuery.matcher(item.getName()).matches())
+                .collect(Collectors.toList());
+    }
 }
